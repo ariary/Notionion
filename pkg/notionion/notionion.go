e366ab8f1a99296cb21c09ea31e970a10f7299c4
ariary <ariary9.2@hotmail.fr>
2022-05-06 04:03:13 -0400

    start button

--- pkg/notionion/notionion.go
+++ pkg/notionion/notionion.go
@@ -2,6 +2,7 @@ package notionion
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	"github.com/jomei/notionapi"
@@ -31,63 +32,78 @@ func RequestProxyStatus(client *notionapi.Client, pageid string) (active bool, e
 }
 
 //GetProxyStatus: get proxy status from page's blocks
-func GetProxyStatus(children notionapi.Blocks) bool {
+func GetProxyStatus(children notionapi.Blocks) (bool, error) {
 	for i := 0; i < len(children); i++ {
 		if children[i].GetType() == "to_do" {
 			todo := children[i].(*notionapi.ToDoBlock).ToDo
 			if todo.RichText[0].Text.Content == "ON" {
-				return todo.Checked
+				return todo.Checked, nil
 			}
 		}
 	}
-	return false
+	err := fmt.Errorf("Failed retrieving proxy status button")
+	return false, err
 }
 
 //GetRequestBlock: retrieve "Request" block from page's blocks
-func GetRequestBlock(children notionapi.Blocks) (requestBlock notionapi.Heading2Block) {
+func GetRequestBlock(children notionapi.Blocks) (requestBlock notionapi.Heading2Block, err error) {
 	for i := 0; i < len(children); i++ {
 		if children[i].GetType() == "heading_2" {
 			headingBlock := children[i].(*notionapi.Heading2Block)
 			if strings.Contains(headingBlock.Heading2.RichText[0].Text.Content, "Request") {
-				return *headingBlock
+				return *headingBlock, nil
 			}
 		}
 	}
-	return requestBlock
+	err = fmt.Errorf("Failed retrieving \"request\" section")
+	return requestBlock, err
 }
 
 //GetResponseBlock: retrieve "Response" block from page's blocks
-func GetResponseBlock(children notionapi.Blocks) (requestBlock notionapi.Heading2Block) {
+func GetResponseBlock(children notionapi.Blocks) (responseBlock notionapi.Heading2Block, err error) {
 	for i := 0; i < len(children); i++ {
 		if children[i].GetType() == "heading_2" {
 			headingBlock := children[i].(*notionapi.Heading2Block)
 			if strings.Contains(headingBlock.Heading2.RichText[0].Text.Content, "Response") {
-				return *headingBlock
+				return *headingBlock, nil
 			}
 		}
 	}
-	return requestBlock
+	err = fmt.Errorf("Failed retrieving \"response\" section")
+	return responseBlock, err
 }
 
-//GetRequestCodeBlock: Obtain the code block object under the request heading
-func GetRequestCodeBlock(children notionapi.Blocks) (requestCodeBlock notionapi.CodeBlock) {
+//GetCodeBlockByName: Obtain the code block object under the section specified by name (name={"Request","Response"})
+func GetCodeBlockByName(children notionapi.Blocks, name string) (requestCodeBlock notionapi.CodeBlock, err error) {
 	for i := 0; i < len(children); i++ {
 		if children[i].GetType() == "code" {
 			codeBlock := children[i].(*notionapi.CodeBlock)
 			if i > 0 {
 				if children[i-1].GetType() == "heading_2" {
 					above := children[i-1].(*notionapi.Heading2Block)
-					if strings.Contains(above.Heading2.RichText[0].Text.Content, "Request") {
-						return *codeBlock
+					if strings.Contains(above.Heading2.RichText[0].Text.Content, name) {
+						return *codeBlock, nil
 					}
 				}
 			}
 		}
 	}
-	return requestCodeBlock
+	err = fmt.Errorf("Failed retrieving request code block within \"request\" section")
+	return requestCodeBlock, err
 }
 
-func UpdateRequestContent(client *notionapi.Client, requestCodeBlockID notionapi.BlockID, request string) (notionapi.Block, error) {
+//GetRequestCodeBlock: Obtain the code block object under the request heading
+func GetRequestCodeBlock(children notionapi.Blocks) (requestCodeBlock notionapi.CodeBlock, err error) {
+	return GetCodeBlockByName(children, "Request")
+}
+
+//GetResponseCodeBlock: Obtain the code block object under the response heading
+func GetResponseCodeBlock(children notionapi.Blocks) (requestCodeBlock notionapi.CodeBlock, err error) {
+	return GetCodeBlockByName(children, "Response")
+}
+
+//UpdateCodeContent: update code block with content
+func UpdateCodeContent(client *notionapi.Client, codeBlockID notionapi.BlockID, content string) (notionapi.Block, error) {
 	//construct code block containing request
 	code := notionapi.CodeBlock{
 		Code: notionapi.Code{
@@ -95,7 +111,7 @@ func UpdateRequestContent(client *notionapi.Client, requestCodeBlockID notionapi
 				{
 					Type: notionapi.ObjectTypeText,
 					Text: notionapi.Text{
-						Content: request,
+						Content: content,
 					},
 					Annotations: &notionapi.Annotations{
 						Bold:          false,
@@ -116,12 +132,69 @@ func UpdateRequestContent(client *notionapi.Client, requestCodeBlockID notionapi
 		Code: &code.Code,
 	}
 
-	return client.Block.Update(context.Background(), requestCodeBlockID, updateReq)
+	return client.Block.Update(context.Background(), codeBlockID, updateReq)
 }
 
-//UpdateResponseContent: update response block
+//GetRequestButtonsColumnBlock: retrieve buttons within request block (column list block)
+func GetRequestButtonsColumnBlock(children notionapi.Blocks) (buttonsBlock notionapi.ColumnListBlock, err error) {
+	for i := 0; i < len(children); i++ {
+		if children[i].GetType() == "column_list" {
+			buttonsBlock := children[i].(*notionapi.ColumnListBlock)
+			if buttonsBlock.HasChildren && i > 0 && children[i-1].GetType() == "code" { //TODO: check if the code is under request heading
+				return *buttonsBlock, nil
+			}
+		}
+	}
+	err = fmt.Errorf("GetRequestButtonsColumnBlock: failed to retrieve column block containg button within request section")
+	return buttonsBlock, err
+}
 
-//CheckRequestSendingBox: Check if the to_do block to send request is checked
+//RequestRequestButtonStatusByName: check if specific to_do block within "request" block is checked.
+// name: {"FORWARD", "DROP"}
+func RequestRequestButtonStatusByName(client *notionapi.Client, pageid string, name string) (checked bool, err error) {
+	children, err := RequestProxyPageChildren(client, pageid)
+	if err != nil {
+		return false, err
+	}
+	buttonsBlock, err := GetRequestButtonsColumnBlock(children)
+	if err != nil {
+		return false, err
+	}
+
+	columnsList, err := client.Block.GetChildren(context.Background(), buttonsBlock.ID, nil)
+	if err != nil {
+		return false, err
+	}
+	columns := columnsList.Results
+	for i := 0; i < len(columns); i++ {
+		buttonsList, err := client.Block.GetChildren(context.Background(), columns[i].GetID(), nil)
+		if err != nil {
+			return false, err
+		}
+
+		for j := 0; j < len(buttonsList.Results); j++ {
+			if buttonsList.Results[j].GetType() == "to_do" {
+				todo := buttonsList.Results[j].(*notionapi.ToDoBlock).ToDo
+				if todo.RichText[0].Text.Content == name {
+					return todo.Checked, err
+				}
+			}
+		}
+	}
+
+	return false, err
+}
+
+func DisableRequestButtons(client *notionapi.Client, pageid string) error {
+	forward, err := RequestRequestButtonStatusByName(client, pageid, "FORWARD")
+	if err != nil {
+		return err
+	}
+
+	//make function requestbutton return button
+	//getstatus use this function and chech "checked" status
+	return nil
+}
 
 // func GetRequestParagraphBlock(children notionapi.Blocks) (requestParagraphBlock notionapi.ParagraphBlock) {
 // 	for i := 0; i < len(children); i++ {
